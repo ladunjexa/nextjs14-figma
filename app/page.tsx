2104e1126d4a5affbd82b02ea185632f66bd41b1
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-05-07 03:04:53 +0300

    chore: configured @fabricjs and @shadcn-ui, added assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
+import clsx from "clsx";
 
 export default function Home() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <div
+      className={clsx(
+        "h-[100vh] w-full flex justify-center items-center",
+        "text-center"
+      )}
+    >
+      <h1 className={clsx("text-2xl text-white")}>Liveblocks Figma</h1>
+    </div>
   );
 }
